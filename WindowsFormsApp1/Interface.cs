787483f9d4e14a3be5db59648c295b4a32423f23
Luzyanin Alexey <261-96@mail.ru>
2018-06-03 15:19:10 +0700

    add some functions

--- WindowsFormsApp1/Interface.cs
+++ WindowsFormsApp1/Interface.cs
@@ -13,7 +13,7 @@ namespace WindowsFormsApp1
 {
     public partial class Interface : Form
     {
-        SqlConnection sqlConnection;        
+        SqlConnection sqlConnection;
 
         public Interface()
         {
@@ -24,7 +24,13 @@ namespace WindowsFormsApp1
         {
             SqlCommand sqlCommand = new SqlCommand("Insert Into [Config] (Nazv, Seria, Soket, TechProc, EnergyPotr, Takt, Yadra) Values(@Nazv, @Seria, @Soket, @TechProc, @EnergyPotr, @Takt, @Yadra)", sqlConnection);
 
-            sqlCommand.Parameters.AddWithValue("Nazv", "hui");
+            sqlCommand.Parameters.AddWithValue("@Nazv", ReadDataGridView.Rows[0].Cells[0].Value);
+            sqlCommand.Parameters.AddWithValue("@Seria", "FX 6300");
+            sqlCommand.Parameters.AddWithValue("@Soket", "AM3+");
+            sqlCommand.Parameters.AddWithValue("@TechProc", 32);
+            sqlCommand.Parameters.AddWithValue("@EnergyPotr", 95);
+            sqlCommand.Parameters.AddWithValue("@Takt", 10);
+            sqlCommand.Parameters.AddWithValue("@Yadra", 6);
         }
 
         private void RandomButton_Click(object sender, EventArgs e)
@@ -34,7 +40,10 @@ namespace WindowsFormsApp1
             sqlConnection.Open();
             SqlDataReader reader = sqlCommand.ExecuteReader();
 
-            ReadDataGridView.Rows.Add();
+            for (int i = 0; i < 9; i++)
+            {
+                ReadDataGridView.Rows.Add(reader.GetValue(1));
+            }
         }
     }
 }
